a23c012a0d6ee436cb7c6f010cfe748ac92ecdd5
Yunahaiwei <980940135@qq.com>
2020-04-30 17:49:31 +0800

    攻略首页

---
+++ components/post/pullDown.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <el-cascader-panel :options="options" :props="{ expandTrigger: 'hover' } " class="hover"></el-cascader-panel>
+
+    <!-- 推荐城市 -->
+    <div class="recommend">
+      <span>推荐城市</span>
+      <img src="http://157.122.54.189:9093/images/pic_sea.jpeg" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      options: [
+        {
+          value: "zhinan",
+          label: "指南",
+          children: [
+            {
+              value: "shejiyuanze",
+              label: "设计原则"
+            },
+            {
+              value: "daohang",
+              label: "导航"
+            }
+          ]
+        }
+      ]
+    };
+  },
+  mounted() {
+    this.$axios({
+      url: "/posts/cities"
+    }).then(res => {
+      const { data } = res.data;
+      console.log(data);
+    });
+  }
+};
+</script>
+
+<style scoped lang="less" >
+/deep/ .el-button {
+  width: 250px;
+  border-radius: nowrap;
+  display: block;
+}
+.el-button--primary {
+  color: black;
+  background-color: #ffffff;
+  border-color: #eeeeee;
+}
+// 推荐城市
+.recommend {
+  span {
+    border-bottom: 1px solid #eeeeee;
+  }
+  img {
+    width: 100px;
+    height: 100px;
+  }
+}
+</style>
--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,13 +1,127 @@
 <template>
-  <div>攻略首页</div>
+  <div class="container">
+    <el-row type="flex" class="row-bg">
+      <!-- 左边 -->
+      <el-col :span="6">
+        <div class="bg-purple">
+          <pulldown />
+        </div>
+      </el-col>
+
+      <!-- 右边 -->
+      <el-col :span="18">
+        <div class="grid-content bg-purple-light">
+          <!-- 输入框 -->
+          <el-input placeholder="请输入你想去的地方，比如'广州'" v-model="input3" class="input-with-select">
+            <el-button slot="append" icon="el-icon-search" class="el-button"></el-button>
+          </el-input>
+          <div class="tuijian">
+            <span>
+              推荐：
+              <a href="#">广州</a>
+              <a href="#">上海</a>
+              <a href="#">北京</a>
+            </span>
+          </div>
+
+          <div class="nav">
+            <div class="strategy">推荐攻略</div>
+            <!-- 游记按钮 -->
+            <el-button type="primary" icon="el-icon-edit">写游记</el-button>
+          </div>
+        </div>
+
+        <!-- 分页 -->
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="currentPage4"
+          :page-sizes="[100, 200, 300, 400]"
+          :page-size="100"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="400"
+        ></el-pagination>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
+import pulldown from "@/components/post/pullDown.vue";
 export default {
-
-}
+  components: {
+    pulldown
+  },
+  data() {
+    return {
+      input3: "",
+      currentPage4: 4
+    };
+  },
+  methods: {
+    handleSizeChange(val) {
+      console.log(`每页 ${val} 条`);
+    },
+    handleCurrentChange(val) {
+      console.log(`当前页: ${val}`);
+    }
+  },
+  mounted() {
+    this.$axios({
+      url: "/posts/cities"
+    }).then(res => {
+      console.log(res);
+    });
+  }
+};
 </script>
 
-<style>
+<style scoped lang="less">
+.container {
+  width: 1000px;
+  margin: 0 auto;
+  height: 800px;
+}
+.el-row {
+  margin-bottom: 20px;
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
 
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.bg-purple {
+  background: #d3dce6;
+}
+.bg-purple-light {
+  background: #e5e9f2;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 500px;
+}
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
+// 搜索框样式
+.input-with-select {
+  border: 4px;
+  color: #ffa500;
+}
+/deep/ .el-input__inner {
+  border: 4px solid #ffa500;
+}
+// 推荐攻略模块.
+.nav {
+  display: flex;
+  border-bottom: 2px solid #eeeeee;
+  justify-content: space-between;
+  .strategy {
+    align-items: center;
+    border-bottom: 2px solid #ffa500;
+  }
+}
 </style>
