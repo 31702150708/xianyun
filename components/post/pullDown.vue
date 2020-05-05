043a041d666fedd324ebf38d1baeb52c9d10bebe
Yunahaiwei <980940135@qq.com>
2020-05-05 10:01:30 +0800

    wancheng

--- components/post/pullDown.vue
+++ components/post/pullDown.vue
@@ -1,32 +1,40 @@
 <template>
-  <div>
-    <div class="liebiao">
+  <div class="menus-body" @mouseleave="cgIndexOut">
+    <div class="menus">
+      <div
+        class="menus-circur"
+        @mouseover="showTabList(index)"
+        @mouseout="hideTabList"
+        :style="{borderRight: currentIndex===index?'none':'', color: currentIndex===index?'orange':''}"
+        v-for="(item, index) in cityList"
+        :key="index"
+      >
+        <el-row type="flex" justify="space-between" align="middle" class="menu-item">
+          <span>{{item.type}}</span>
+          <i class="el-icon-arrow-right"></i>
+        </el-row>
+      </div>
+    </div>
+    <div
+      class="sub-menus"
+      v-show="isShow"
+      @mouseover="showTabList(currentIndex)"
+      @mouseleave="hideTabListAll"
+    >
       <ul>
-        <li>
-          热门城市
-          <i class="el-icon-arrow-right"></i>
-        </li>
-        <li>
-          热门城市
-          <i class="el-icon-arrow-right"></i>
-        </li>
-        <li>
-          热门城市
-          <i class="el-icon-arrow-right"></i>
-        </li>
-        <li>
-          热门城市
-          <i class="el-icon-arrow-right"></i>
+        <li v-for="(item,index) in tabContent" :key="item.city + index">
+          <i>{{index + 1}}</i>
+          <strong @click="sendCityName(item.city)">{{item.city}}</strong>
+          <span @click="sendCityName(item.city)">{{item.desc}}</span>
         </li>
       </ul>
     </div>
 
-    <!-- 推荐城市 -->
-    <div class="recommend">
-      <span class="tuijian">推荐城市</span>
-      <a href="#">
-        <img src="http://157.122.54.189:9093/images/pic_sea.jpeg" />
-      </a>
+    <div class="aside-recommend" @mouseenter="cgIndex">
+      <nuxt-link to="/post#">
+        <h4>推荐城市</h4>
+        <img src="http://157.122.54.189:9093/images/pic_sea.jpeg" alt />
+      </nuxt-link>
     </div>
   </div>
 </template>
@@ -35,55 +43,154 @@
 export default {
   data() {
     return {
-      options: [
-        {
-          value: "zhinan",
-          label: "指南"
-        }
-      ]
+      cityList: [],
+      currentIndex: 4,
+      isShow: false,
+      time: 0
     };
   },
+  computed: {
+    tabContent() {
+      if (!this.cityList[this.currentIndex]) {
+        return [];
+      }
+      return this.cityList[this.currentIndex].children;
+    }
+  },
   mounted() {
-    this.$axios({
-      url: "/posts/cities"
-    }).then(res => {
-      const { data } = res.data;
-      console.log(data);
-    });
+    this.getCityData();
+  },
+  methods: {
+    getCityData() {
+      this.$axios({
+        url: "/posts/cities"
+      }).then(({ data }) => {
+        this.cityList = data.data;
+      });
+    },
+    showTabList(index) {
+      this.currentIndex = index;
+      this.isShow = true;
+    },
+    hideTabList(index) {
+      this.isShow = false;
+    },
+    sendCityName(data) {
+      this.$emit("gainCityName", data);
+    },
+    cgIndexOut() {
+      this.isShow = false;
+      this.currentIndex = 4;
+    },
+    hideTabListAll() {
+      this.isShow = false;
+      this.currentIndex = 4;
+    },
+    cgIndex() {
+      this.currentIndex = 4;
+    }
   }
 };
 </script>
 
-<style scoped lang="less" >
-// 列表
-.liebiao {
-  margin-top: 15px;
-  ul {
-    li {
-      width: 100%;
-      height: 40px;
-      line-height: 40px;
-      border: 1px solid #eeeeee;
-      padding-left: 20px;
-      font-size: 14px;
+<style lang="less" scoped>
+.menus-body {
+  position: relative;
+}
+.menus {
+  width: 260px;
+  border-bottom: 1px solid #ddd;
+  .menus-circur {
+    box-sizing: border-box;
+    width: 260px;
+    height: 41px;
+    padding: 0 20px;
+    font-size: 14px;
+    border: 1px solid #ddd;
+    border-bottom: none;
+
+    &:hover {
+      border-right: none;
+      span {
+        color: orange;
+      }
+      i {
+        color: orange;
+      }
     }
   }
 }
-// 推荐城市
-.recommend {
-  span {
+.menu-item {
+  width: 100%;
+  height: 100%;
+}
+.sub-menus {
+  box-sizing: border-box;
+  width: 350px;
+  padding: 10px 20px;
+  position: absolute;
+  right: -319px;
+  top: 0;
+  border: 1px solid #ddd;
+  border-left: none;
+  background-color: #fff;
+  z-index: 999;
+  &::before {
+    content: "";
     display: block;
-    width: 100%;
-    height: 40px;
-    line-height: 40px;
-    border-bottom: 1px solid #eeeeee;
-    padding-top: 15px;
+    width: 1px;
+    height: 53px;
+    background-color: #ddd;
+    position: absolute;
+    bottom: 0;
+    left: -1px;
+  }
+  ul {
+    li {
+      vertical-align: middle;
+      font-size: 14px;
+      left: 36px;
+      line-height: 36px;
+    }
+  }
+  i {
+    position: relative;
+    top: 3px;
+    color: orange;
+    font-size: 24px;
+    font-style: italic;
+  }
+  strong {
+    margin: 0 10px;
+    color: orange;
+    font-weight: 400;
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+  span {
+    color: #999;
+    cursor: pointer;
+    &:hover {
+      text-decoration: underline;
+      color: orange;
+    }
+  }
+}
+.aside-recommend {
+  width: 260px;
+  margin-top: 20px;
+  h4 {
+    font-weight: 400;
+    padding-bottom: 10px;
+    border-bottom: 1px solid #ddd;
+    margin-bottom: 10px;
   }
   img {
-    display: flex;
+    display: block;
     width: 100%;
-    height: 100%;
-    margin-top: 10px;
+    cursor: pointer;
   }
 }
 </style>
--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,45 +1,50 @@
 <template>
-  <div class="container">
-    <el-row type="flex" class="row-bg">
-      <!-- 左边 -->
-      <el-col :span="6">
-        <div>
-          <pulldown />
+  <div class="post">
+    <el-row>
+      <el-col :span="7">
+        <div class="post-aside">
+          <pullDown @gainCityName="gainCityName" />
         </div>
       </el-col>
 
-      <!-- 右边 -->
-      <el-col :span="18" class="youbian">
-        <div class="grid-content">
-          <!-- 输入框 -->
-          <el-input placeholder="请输入你想去的地方，比如'广州'" v-model="input3" class="input-with-select">
-            <el-button slot="append" icon="el-icon-search" class="el-button"></el-button>
+      <el-col :span="17" class="post-main">
+        <div class="post-search">
+          <el-input
+            placeholder="请输入想去的地方，比如 '广州'"
+            v-model="input3"
+            class="input-with-select"
+            @change="searchArticleList"
+          >
+            <i
+              class="el-icon-search el-input__icon post-icon"
+              slot="suffix"
+              @click="searchArticleListInput"
+            ></i>
           </el-input>
-          <div class="tuijian">
-            <span>
-              推荐：
-              <a href="#">广州</a>
-              <a href="#">上海</a>
-              <a href="#">北京</a>
-            </span>
+          <div class="post-recommend">
+            推荐：&nbsp;
+            <span
+              v-for="(item,index) in recommendList"
+              :key="index"
+              @click="addCityName(item)"
+            >{{item}}</span>
           </div>
-
-          <div class="nav">
-            <div class="strategy">推荐攻略</div>
-            <!-- 游记按钮 -->
-            <el-button type="primary" icon="el-icon-edit">写游记</el-button>
+          <div class="post-raiders">
+            <el-row type="flex" justify="space-between" align="middle">
+              <h3>推荐攻略</h3>
+              <el-button type="primary" icon="el-icon-edit" @click="toWriteArticle">写游记</el-button>
+            </el-row>
           </div>
         </div>
-
-        <!-- 分页 -->
+        <wenzhang v-for="(item,index) in articleList" :key="item.id+index" :cardContent="item"></wenzhang>
         <el-pagination
           @size-change="handleSizeChange"
           @current-change="handleCurrentChange"
-          :current-page="currentPage4"
-          :page-sizes="[5, 10, 15, 20]"
-          :page-size="100"
+          :current-page="start / limit +1"
+          :page-sizes="[3,5,10,15]"
+          :page-size="limit"
           layout="total, sizes, prev, pager, next, jumper"
-          :total="40"
+          :total="total"
         ></el-pagination>
       </el-col>
     </el-row>
@@ -47,94 +52,131 @@
 </template>
 
 <script>
-import pulldown from "@/components/post/pullDown.vue";
+import pullDown from "@/components/post/pullDown";
+import wenzhang from "@/components/post/wenzhang";
 export default {
   components: {
-    pulldown
+    pullDown,
+    wenzhang
   },
   data() {
     return {
       input3: "",
-      currentPage4: 4
+      recommendList: ["广州", "上海", "北京"],
+      articleList: [],
+      start: 0,
+      limit: 5,
+      total: 0
     };
   },
   methods: {
-    handleSizeChange(val) {
-      console.log(`每页 ${val} 条`);
+    toWriteArticle() {
+      this.$router.push({
+        path: "/post/create"
+      });
     },
-    handleCurrentChange(val) {
-      console.log(`当前页: ${val}`);
+    searchArticleList() {
+      if (this.input3) {
+        params.city = this.input3;
+      }
+      this.$axios({
+        url: "/posts",
+        params: {
+          _start: this.start, // 从哪一条数据开始
+          _limit: this.limit // 拿多少条数据
+        }
+      }).then(res => {
+        const { data, total } = res.data;
+        this.articleList = data;
+        this.total = total;
+      });
+    },
+    handleSizeChange(data) {
+      this.pagesizes = data;
+      this.searchArticleList();
+    },
+    handleCurrentChange(index) {
+      this.start = (index - 1) * this.limit;
+      this.$router.replace({
+        url: this.$route.path,
+        query: {
+          start: this.st
+        }
+      });
+    },
+
+    gainCityName(data) {
+      this.input3 = data;
+      this.currentIndex = 1;
+      this.searchArticleList();
+    },
+    searchArticleListInput() {
+      this.currentIndex = 1;
+      this.searchArticleList();
     }
   },
   mounted() {
-    this.$axios({
-      url: "/posts/cities"
-    }).then(res => {
-      console.log(res);
-    });
+    this.searchArticleList();
+  },
+  beforeRouteUpdate(to, form, next) {
+    next();
+    this.currentIndex = 1;
+    this.input3 = "";
+    this.searchArticleList();
+    console.log(to);
   }
 };
 </script>
 
-<style scoped lang="less">
-.container {
+<style lang="less" scoped>
+.post {
   width: 1000px;
+  padding: 20px 0;
   margin: 0 auto;
-  height: 800px;
-  .youbian {
-    margin-left: 40px;
-    margin-top: 10px;
-  }
 }
-.el-row {
-  margin-bottom: 20px;
-  &:last-child {
-    margin-bottom: 0;
-  }
+.post-main {
+  padding-left: 9px;
 }
-
-.bg-purple-dark {
-  background: #99a9bf;
+.post-icon {
+  position: relative;
+  left: -6px;
+  font-size: 22px;
+  color: orange;
 }
-
-.bg-purple-light {
-  background: #e5e9f2;
+/deep/.el-input__inner {
+  border: 3px solid orange;
+  color: #7575a1;
+  font-size: 14px;
 }
-.grid-content {
-  border-radius: 4px;
-  min-height: 500px;
-}
-.row-bg {
+.post-recommend {
   padding: 10px 0;
-  // background-color: #f9fafc;
-}
-// 搜索框样式
-.input-with-select {
-  border: 4px;
-  color: #ffa500;
-}
-/deep/ .el-input__inner {
-  border: 4px solid #ffa500;
-}
-// 推荐攻略模块.
-.nav {
-  display: flex;
-  border-bottom: 2px solid #eeeeee;
-  justify-content: space-between;
-  margin-top: 20px;
-  .strategy {
-    align-items: center;
-    border-bottom: 2px solid #ffa500;
+  font-size: 12px;
+  color: #666;
+  span {
+    margin-right: 5px;
+    &:hover {
+      color: orange;
+      text-decoration: underline;
+      cursor: pointer;
+    }
   }
 }
-// 输入框模块
-.grid-content {
-  .tuijian {
-    font-size: 12px;
-    color: #858585;
-    margin-top: 10px;
-    a {
-      margin-right: 10px;
+.post-raiders {
+  padding-bottom: 10px;
+  border-bottom: 1px solid #eee;
+  h3 {
+    color: orange;
+    font-size: 18px;
+    font-weight: 400;
+    &::after {
+      content: "";
+      display: block;
+      width: 72px;
+      height: 2px;
+      background: orange;
+      position: absolute;
+      bottom: -11px;
+      left: 0;
     }
   }
 }
